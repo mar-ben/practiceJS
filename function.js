392f2a193800425a8ac4af2def4db7e3f9afa2a3
marben <mailbenhar@gmail.com>
2019-12-21 12:34:57 -0600

    updated function.js

--- function.js
+++ function.js
@@ -39,3 +39,92 @@ function minus(a,b){
 }
 console.log('10-5=',minus(10,5));
 console.log('-10='+ minus(10));
+
+//clouse -function has access to variables defined in it scope.
+
+function wrapValue(n)
+{
+    let local=n;
+    return ()=> local;
+}
+let wrap1=wrapValue(1);
+let wrap2=wrapValue(2);
+
+console.log(wrap1);
+console.log(wrap2);
+
+function multiplier(factor){
+    return number=> number*factor;
+}
+let twice=multiplier(2);
+console.log(twice(5));
+console.log(twice(2));
+
+//side efects 
+const compute=function(x,y){
+    console.log(x*y);
+    return x*y;
+}
+compute(10,2);
+
+//pure function - function without side efects.
+
+const compute2=function(x,y){
+    return x*y;
+}
+console.log(compute2(2,6));
+
+//excercise find min
+
+const min=function (a,b){
+    return (a<b)?a:b;
+}
+console.log(min(2,3));
+console.log(min(3,2));
+console.log(min(2,2));
+
+//Excersise recursion -find even Number.
+console.log('Excercise is Even');
+
+const isEven=function(n){
+    //console.log(n);
+    if(n>=2){
+        return isEven(n-2);
+    }
+    if(n==1){
+        return false;
+    }
+    if(n==0){
+        return true;
+    }
+}
+console.log(isEven(4));
+console.log(isEven(3));
+
+console.log('Excercise - bean counting ');
+
+const countBs=function(str)
+{
+    let count=0;
+    for(let a=0;a<str.length;a++){
+        if(str[a]==='B'){
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countBs('BBC'));
+
+console.log('Excersise - count character in string');
+
+const countChar=function(str,char){
+    let count=0;
+    for(let a=0;a<str.length; a++){
+        if(str[a]===char){
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countChar('kakkerlak','k'));
+
